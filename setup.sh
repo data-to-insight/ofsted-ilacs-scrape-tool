07e8df01f777ad13a36eb5871b7325c1668d3571
Rob Harrison <48765695+robjharrison@users.noreply.github.com>
2024-04-08 12:09:42 +0000

    data refresh 080424 + scipy fix

--- setup.sh
+++ setup.sh
@@ -6,6 +6,8 @@ sudo apt-get update
 # Install Python dependencies
 pip install -r requirements.txt
 
+
                                      
 # Install the Python extension for Visual Studio Code
 code --install-extension ms-python.python --force
+
