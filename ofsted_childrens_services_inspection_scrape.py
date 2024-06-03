2eb95a67b1ee81f2ae9fd47592d85420351998d4
Rob Harrison <48765695+robjharrison@users.noreply.github.com>
2024-06-03 13:22:42 +0000

    server timestamp correction +1h fix

--- ofsted_childrens_services_inspection_scrape.py
+++ ofsted_childrens_services_inspection_scrape.py
@@ -71,7 +71,7 @@ import pandas as pd
 import numpy as np
 from bs4 import BeautifulSoup
 import re       
-from datetime import datetime
+from datetime import datetime, timedelta # timedelta enables server time adjustment
 import json
 import git
 
@@ -1226,13 +1226,18 @@ def save_to_html(data, column_order, local_link_column=None, web_link_column=Non
         # remove the inspection_reports_folder path prefix from the file paths, create last updated list 
         las_with_new_inspection_list = [os.path.relpath(file, inspection_reports_folder) for file in changed_files]
 
-        
+        # print(las_with_new_inspection_list)
+
     except Exception as e:
         print(f"Error processing repository: {e}")
         raise
 # end of Most-recent-reports generate
 # Chk + remove onward use of variable las_with_new_inspection_list IF running locally
 
+    
+
+    # current time, add one hour to the current time to correct git server time
+    adjusted_timestamp_str = (datetime.now() + timedelta(hours=1)).strftime("%d %m %Y %H:%M")
 
     # init HTML content with title and CSS
     html_content = f"""
@@ -1263,7 +1268,7 @@ def save_to_html(data, column_order, local_link_column=None, web_link_column=Non
         <h1>{page_title}</h1>
         <p>{intro_text}</p>
         <p>{disclaimer_text}</p>
-        <p><b>Summary data last updated: {datetime.now().strftime("%d %m %Y %H:%M")}</b></p>
+        <p><b>Summary data last updated: {adjusted_timestamp_str}</b></p>
         <p><b>LA inspections last updated: {las_with_new_inspection_list}</b></p>
         <div class="container">
     """
