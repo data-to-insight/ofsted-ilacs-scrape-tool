4a1ecb8693c5700c2ef9b9dc931f01f0e155867d
Rob Harrison <48765695+robjharrison@users.noreply.github.com>
2025-01-15 10:51:27 +0000

    Data refresh 150125

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 # Install additional system-level dependencies/packages
 sudo apt-get update    
+python3 -m pip install --upgrade pip
 
 # Install Python dependencies
 pip install -r requirements.txt
