12ade5baedbab9a856771fc3456b567ca165a3d8
Rob Harrison <48765695+robjharrison@users.noreply.github.com>
2025-03-10 09:50:08 +0000

    minor txt content chg

--- ofsted_ilacs_scrape.py
+++ ofsted_ilacs_scrape.py
@@ -1163,7 +1163,7 @@ def save_to_html(data, column_order, local_link_column=None, web_link_column=Non
     # )
 
     intro_text = f"""
-    Summarised outcomes of published short and standard ILACS inspection reports by Ofsted, refreshed daily.<br/>
+    Summarised outcomes of published short and standard ILACS inspection reports by Ofsted, refreshed weekly.<br/>
     An expanded version of the shown summary sheet, refreshed concurrently, is available to
     <a href="{export_summary_filename}.xlsx">download here</a> as an .xlsx file.
     <br/>Data summary is based on the original <i>ILACS Outcomes Summary</i> published periodically by the ADCS:
