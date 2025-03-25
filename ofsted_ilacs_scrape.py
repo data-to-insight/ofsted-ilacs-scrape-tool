0a32565a92d62d582017214235d25c5186affb0e
Rob Harrison <48765695+robjharrison@users.noreply.github.com>
2025-03-25 10:11:58 +0000

    minor tweaks

--- ofsted_ilacs_scrape.py
+++ ofsted_ilacs_scrape.py
@@ -27,17 +27,18 @@ pdf_data_capture = True # True is default (scrape within pdf inspection reports 
 
 
 
+
 # Needed towards git actions workflow
-# Use GITHUB_WORKSPACE environment variable if available,
-# otherwise fall back to the default path.
-repo = os.environ.get('GITHUB_WORKSPACE', '/workspaces/ofsted-ilacs-scrape-tool')
+# Use GITHUB_WORKSPACE env var if available(workflow actions), 
+# otherwise fall back to the default path(codespace).
+repo = os.environ.get('GITHUB_WORKSPACE', '/workspaces/ofsted-jtai-scrape-tool')
 
 try:
     repo_path = git.Repo(repo)
 except git.exc.NoSuchPathError:
     print(f"Error initialising repo path for inspection reports: {repo}")
-    # Handle the error as needed, for example, exit or use an alternative approach.
     raise
+
 
 
 
